55378037ad9100db8c1174b6047476ce61053b75
Neil Mackay <neilmackay01@gmail.com>
2019-08-09 14:57:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
 
     if ($_POST['submit']) {
         if ($name != '' && $email != '') {
-            if ($human == '4') {				 
+            if ($human == '5') {				 
                 if (mail ($to, $subject, $body, $from)) { 
                 echo '<p>Your message has been sent!</p>';
             } else { 
                 echo '<p>Something went wrong, go back and try again!</p>'; 
             } 
-        } else if ($_POST['submit'] && $human != '4') {
+        } else if ($_POST['submit'] && $human != '5') {
             echo '<p>You answered the anti-spam question incorrectly!</p>';
         }
         } else {
