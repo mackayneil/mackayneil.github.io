9204c4e93afb188fa2b4f7ff25c9c4dab2c1e497
Neil Mackay <neilmackay01@gmail.com>
2019-08-09 14:55:21 +0200

    update php.index

--- index.php
+++ index.php
@@ -8,22 +8,20 @@
     $human = $_POST['human'];
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-?>
 
-<?php
-if ($_POST['submit']) {
-    if ($name != '' && $email != '') {
-        if ($human == '4') {				 
-            if (mail ($to, $subject, $body, $from)) { 
-	        echo '<p>Your message has been sent!</p>';
-	    } else { 
-	        echo '<p>Something went wrong, go back and try again!</p>'; 
-	    } 
-	} else if ($_POST['submit'] && $human != '4') {
-	    echo '<p>You answered the anti-spam question incorrectly!</p>';
-	}
-    } else {
-        echo '<p>You need to fill in all required fields!!</p>';
+    if ($_POST['submit']) {
+        if ($name != '' && $email != '') {
+            if ($human == '4') {				 
+                if (mail ($to, $subject, $body, $from)) { 
+                echo '<p>Your message has been sent!</p>';
+            } else { 
+                echo '<p>Something went wrong, go back and try again!</p>'; 
+            } 
+        } else if ($_POST['submit'] && $human != '4') {
+            echo '<p>You answered the anti-spam question incorrectly!</p>';
+        }
+        } else {
+            echo '<p>You need to fill in all required fields!!</p>';
+        }
     }
-}
 ?>
