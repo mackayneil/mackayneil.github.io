15ec4811182e98005e034e66b8fb12c6b7555a6d
Neil Mackay <neilmackay01@gmail.com>
2019-08-09 17:26:52 +0200

    Form

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<!-- Used for when site is on a hosted platform as Github doesn't support PHP
+Just need to add action="index.php"  and  
+<label>*What is 2+4? (Anti-spam)</label> 
+<input name="human" placeholder="Type Here">
+to form -->
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -7,21 +12,16 @@
     $subject = 'New contact submission';
     $human = $_POST['human'];
 
+    		
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
-    if ($_POST['submit']) {
-        if ($name != '' && $email != '') {
-            if ($human == '5') {				 
-                if (mail ($to, $subject, $body, $from)) { 
-                echo '<p>Your message has been sent!</p>';
-            } else { 
-                echo '<p>Something went wrong, go back and try again!</p>'; 
-            } 
-        } else if ($_POST['submit'] && $human != '5') {
-            echo '<p>You answered the anti-spam question incorrectly!</p>';
-        }
-        } else {
-            echo '<p>You need to fill in all required fields!!</p>';
-        }
+				
+    if ($_POST['submit'] && $human == '4') {				 
+        if (mail ($to, $subject, $body, $from)) { 
+	    echo '<p>Your message has been sent!</p>';
+	} else { 
+	    echo '<p>Something went wrong, go back and try again!</p>'; 
+	} 
+    } else if ($_POST['submit'] && $human != '4') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
     }
 ?>
